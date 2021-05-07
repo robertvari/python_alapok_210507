08483bccf99bbec1c5cac5f9b713180b1229db67
Robert Vári <mail.robertvari@gmail.com>
2021-05-07 13:57:34 +0200

    update

--- 06_dictionary.py
+++ 06_dictionary.py
@@ -2,8 +2,19 @@ phonebook = {
     "06206667788": {"name": "Robert", "address": "Budapest"},
     "06206667733": {"name": "Tamás", "address": "Debrecen"},
     "06206667711": {"name": "Csilla", "address": "Pécs"},
-    "numbers": [1, 2, 3, 4, 5]
+    "numbers": [1, 2, 3, 4]
 }
 
 print(phonebook["06206667711"]["address"])
-print(phonebook["numbers"][-1])
+
+phonebook["06206661111"] = {"name": "Kriszta", "address": "Nyíregyháza"}
+print(phonebook)
+
+
+phonebook_list = [
+    {"phone": "01234", "name": "Robert"},
+    {"phone": "5678", "name": "Csaba"},
+    {"phone": "5678", "name": "Csaba"},
+]
+
+print(phonebook_list[-1]["name"])
