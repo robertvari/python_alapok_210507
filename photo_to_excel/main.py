f84cd0cb87c56570a15d24bc8978d0f8a824cd52
Robert Vári <mail.robertvari@gmail.com>
2021-05-11 14:14:06 +0200

    photo to excel

--- photo_to_excel/main.py
+++ photo_to_excel/main.py
@@ -1,17 +1,8 @@
 from PIL import Image
 import os
 
-ext_list = [".jpg", ".jpeg", ".png"]
+ext_list = [".jpg", ".jpeg"]
 folder_path = r"C:\Work\_PythonSuli\python_alapozo\photos"
-file_list = os.listdir(folder_path)
 
-photo_list = [ i for i in file_list if ".jpg" in i.lower()]
-
-
-# for i in file_list:
-#     name, ext = os.path.splitext(i)
-#     if ext.lower() not in ext_list:
-#         continue
-#     photo_list.append(i)
-
-print(photo_list)
+file_list = [i for i in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, i))]
+photo_list = [i for i in file_list if os.path.splitext(i)[1].lower() in ext_list]
